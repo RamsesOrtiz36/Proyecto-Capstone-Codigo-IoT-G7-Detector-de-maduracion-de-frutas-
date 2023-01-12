ac42e99b66c9da69d58f7d4f53dcea1c4a87f23c
Ramsés Ortiz Castro <Ramses.ortiz36@gmail.com>
2023-01-11 22:24:13 -0700

    Recibe argumento nombre de fruta y entrega calibra

--- Detector_de_color_Python_y_ESP32CAM/Ciclo_for_Medicion.py
+++ Detector_de_color_Python_y_ESP32CAM/Ciclo_for_Medicion.py
@@ -7,7 +7,9 @@ pasarlos a una variable de python y de esos registros tomar del parametro HSV lo
 de HSV minimos y maximos de calibraciones, estos estan en formato string y se psasn a formato JSON,
 en python se maneja como diccionario el arreglo {key:value, key:value,..}
 accedimos a cada "value" usando su "key".
-
+El 11 de enero agregue la biblioteca de argparse para poder usar argumentos desde node red dentro de
+el programa de python, tambien comprobe que el resultado de print() en python sale como output del node 
+exe en node-red.
 """
 import paho.mqtt.client as mqtt
 import paho.mqtt.subscribe as subscribe
@@ -16,7 +18,9 @@ import numpy as np                                                              
 import json                                                                         #Biblioteca para manejar JSON
 import cv2
 import urllib.request
+import argparse
 
+#MySQL 
 conexion1=mysql.connector.connect(host="localhost",                                 #Establece la conexión a MySQL
                                   user="Ramses",                                    #Nombre de usuario con permiso para acceder
                                   passwd="Tanis36",                                 #Contraseña para acceder a MySQL de forma remota
@@ -28,11 +32,23 @@ def nothing(x):
 global AreaMax
 #AreaMax=0
 
+#Argumentos desde Node-Red
+# Parser
+parser = argparse.ArgumentParser()
+parser.add_argument("NombreFruta", help="Nombre de la fruta seleccionada desde Node red")
+#parser.add_argument("db_path", help="Ruta de la base de datos de caras")
+args = parser.parse_args()
 
+Fruta = args.NombreFruta
+
+print ("El nombre de fruta recibida es: " + Fruta)
+
+
+#MQTT
 """msg = subscribe.simple("codigoIoT/NodeRed/Python/Medicion", hostname='localhost')         #Establece la subscripciona  tema de MQTT
 print("%s %s" % (msg.topic, msg.payload))  """                                             #Imprime el tema y elcontenido del msg.payload
 Mediciones= "Msg Payload"      #Variable datos_calibración contiene un json de la base de datos MySQL 
-NombreFrutaPy = "Fruta1"
+NombreFrutaPy = Fruta
 print(NombreFrutaPy)
 if Mediciones!="":
     cursor1.execute("SELECT id, HSV,DiasRest FROM Calibraciones WHERE fruta='"+NombreFrutaPy+"'")    #Petición a MySQL para tomar los valores de los registros de la tabla que cumplen los requisitos
